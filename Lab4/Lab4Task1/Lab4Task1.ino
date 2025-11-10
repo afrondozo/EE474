6256a53cf9dc1ee6c1a06a067819065e37298b4d
afrondozo <106889542+afrondozo@users.noreply.github.com>
2025-11-10 14:48:16 -0800

    Added skeleton code for Task 1

--- Lab4/Lab4Task1/Lab4Task1.ino
+++ Lab4/Lab4Task1/Lab4Task1.ino
@@ -1,3 +1,36 @@
+// Total times for tasks
+const TickType_t ledTaskExecutionTime = 500 / portTICK_PERIOD_MS;      // 500 ms
+const TickType_t counterTaskExecutionTime = 2000 / portTICK_PERIOD_MS; // 2 seconds
+const TickType_t alphabetTaskExecutionTime = 13000 / portTICK_PERIOD_MS; // 13 seconds
+// Remaining Execution Times
+volatile TickType_t remainingLedTime = ledTaskExecutionTime;
+volatile TickType_t remainingCounterTime = counterTaskExecutionTime;
+volatile TickType_t remainingAlphabetTime = alphabetTaskExecutionTime;
+
+
+void ledTask(void *arg) {
+   // TODO: Blink an LED and update remaining time for this task
+}
+
+
+void counterTask(void *arg) {
+ // TODO: Print out an incrementing counter to your LCD, and 
+ //       update remaining time for this task
+}
+
+
+void alphabetTask(void *arg) {
+ // TODO: Print out the alphabet to Serial, and update remaining
+ //       time for this task
+}
+
+
+void scheduleTasks(void *arg) {
+   // TODO: Implement SRTF scheduling logic. This function should select the task with 
+   //       the shortest remaining time and run it. Once a task completes it should 
+   //       reset its remaining time.
+}
+
 void setup() {
   // put your setup code here, to run once:
 

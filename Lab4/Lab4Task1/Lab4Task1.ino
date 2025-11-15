a50239da9aa3ac0e41690d170652ec5ab004a8a1
knneal <kneal07@uw.edu>
2025-11-14 16:44:38 -0800

    lab 4 task 1

--- Lab4/Lab4Task1/Lab4Task1.ino
+++ Lab4/Lab4Task1/Lab4Task1.ino
@@ -6,6 +6,7 @@
 
 #define LED_PIN 5
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define ADDR 0x27
 
 // Total times for tasks
 const TickType_t ledTaskExecutionTime = 500 / portTICK_PERIOD_MS;      // 500 ms
@@ -15,79 +16,109 @@ const TickType_t alphabetTaskExecutionTime = 13000 / portTICK_PERIOD_MS; // 13 s
 volatile TickType_t remainingLedTime = ledTaskExecutionTime;
 volatile TickType_t remainingCounterTime = counterTaskExecutionTime;
 volatile TickType_t remainingAlphabetTime = alphabetTaskExecutionTime;
+// Task Handles
+TaskHandle_t ledTaskHandle = NULL;
+TaskHandle_t counterTaskHandle = NULL;
+TaskHandle_t alphaTaskHandle = NULL;
 
 
 void ledTask(void *arg) {
-  remainingLedTime = ledTaskExecutionTime;
-  while (remainingLedTime > 0) {
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    vTaskDelay(pdMS_TO_TICKS(100));
-    remainingLedTime -= 100;
+  while (1) {
+    if (remainingLedTime > 0) {
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+      remainingLedTime -= pdMS_TO_TICKS(100);
+    }
+    vTaskSuspend(NULL);
   }
 }
 
 void counterTask(void *arg) {
- // TODO: Print out an incrementing counter to your LCD, and 
- //       update remaining time for this task
-  remainingCounterTime = counterTaskExecutionTime;
+  int count = 1;
   while (1) {
-    sendCommand(0x01); // clear lcd
-    sendCommand(0x80); // set cursor to beg
-
-    for (int i = 1; i < 10; i++) {
-      if (remainingCounterTime > 0) {
-        lcd.print('0' + i); 
-        vTaskDelay(pdMS_TO_TICKS(100));
-        remainingCounterTime -= 100;
-      }
-    }
-
-    for (int i = 0; i < 10; i++) {
-      if (remainingCounterTime > 0) {
-        lcd.print('1');
-        lcd.print('0' + i); 
-        vTaskDelay(pdMS_TO_TICKS(100));
-        remainingCounterTime -= 100;
-      }
-    }
-
     if (remainingCounterTime > 0) {
-        lcd.print('2');
-        lcd.print('0'); 
-        vTaskDelay(pdMS_TO_TICKS(100));
-        remainingCounterTime -= 100;
-    } 
+      // lcd.setCursor(0, 0);
+      if (count == 13) {
+        lcd.setCursor(0, 1);
+      }
+      lcd.print(count);
+      //lcd.print(' ');
+  
+      count = count % 20 + 1; 
+
+      remainingCounterTime -= pdMS_TO_TICKS(100);
+    }
+    vTaskSuspend(NULL);
   }
 }
 
 void alphabetTask(void *arg) {
- // TODO: Print out the alphabet to Serial, and update remaining
- //       time for this task
-  remainingAlphabetTime = alphabetTaskExecutionTime;
+  char letter = 'A';
   while (1) {
-    for (char c = 'A'; c <= 'Z'; c++) {
-      if (remainingAlphabetTime > 0)
-        Serial.print(c);
-        Serial.print(' ');
-        vTaskDelay(pdMS_TO_TICKS(500));
-        remainingCounterTime -= 500;
-      }
+    if (remainingAlphabetTime > 0) {
+      Serial.print(letter);
+      Serial.print(' ');
+
+      letter++;
+      if (letter > 'Z') letter = 'A';
+
+      remainingAlphabetTime -= pdMS_TO_TICKS(500);
     }
-    Serial.println();
+    vTaskSuspend(NULL);
   }
 }
 
 void scheduleTasks(void *arg) {
-   // TODO: Implement SRTF scheduling logic. This function should select the task with 
-   //       the shortest remaining time and run it. Once a task completes it should 
-   //       reset its remaining time.
+  while (1) {
+    // Grab current remaining times
+    TickType_t currLed = remainingLedTime;
+    TickType_t currCounter = remainingCounterTime;
+    TickType_t currAlpha = remainingAlphabetTime;
+
+    if (currLed <= 0 && currCounter <= 0 && currAlpha <= 0) { // reset if all done !!
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      Serial.println();
+
+      remainingLedTime = ledTaskExecutionTime;
+      remainingCounterTime = counterTaskExecutionTime;
+      remainingAlphabetTime = alphabetTaskExecutionTime;
+    }
+
+    // Determine which task to run
+    if (currLed > 0 && (currLed <= currCounter || currCounter <= 0) && (currLed <= currAlpha || currAlpha <= 0)) {
+      vTaskResume(ledTaskHandle);
+      vTaskDelay(pdMS_TO_TICKS(100));
+    } else if (currCounter > 0 && (currCounter <= currLed || currLed <= 0) && (currCounter <= currAlpha || currAlpha <= 0)) {
+      vTaskResume(counterTaskHandle);
+      vTaskDelay(pdMS_TO_TICKS(100));
+    } else if (currAlpha > 0 && (currAlpha <= currCounter || currCounter <= 0) && (currAlpha <= currLed || currLed <= 0)) {
+      vTaskResume(alphaTaskHandle);
+      vTaskDelay(pdMS_TO_TICKS(500));
+    }
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
 }
 
 void setup() {
+  // Initializations
+  Wire.begin();
   lcd.init();
-  lcd.display();
+  lcd.backlight();
   delay(2);
   pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+
+  xTaskCreate(ledTask, "LED", 4096, NULL, 1, &ledTaskHandle);
+  xTaskCreate(counterTask, "Counter", 4096, NULL, 1, &counterTaskHandle);
+  xTaskCreate(alphabetTask, "Alphabet", 4096, NULL, 1, &alphaTaskHandle);
+
+  // Suspend all tasks at beginning
+  vTaskSuspend(ledTaskHandle);
+  vTaskSuspend(counterTaskHandle);
+  vTaskSuspend(alphaTaskHandle);
+
+  // Create scheduler to manage the other tasks
+  xTaskCreate(scheduleTasks, "Scheduler", 4096, NULL, 2, NULL);
 
 }
 
